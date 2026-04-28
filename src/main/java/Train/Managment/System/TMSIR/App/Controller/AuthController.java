f9a958e385ad2a4e83144875efe02c7772aafe77
j-A-b1 <jagbho3188@gmail.com>
2026-04-29 02:16:52 +0530

    After app is  live

--- src/main/java/Train/Managment/System/TMSIR/App/Controller/AuthController.java
+++ src/main/java/Train/Managment/System/TMSIR/App/Controller/AuthController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "http://127.0.0.1:5500")
+@CrossOrigin(origins = "*")
 
 public class AuthController {
     @Autowired
--- src/main/java/WebConfig.java
+++ src/main/java/WebConfig.java
@@ -7,7 +7,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Allow all paths
-                .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500") // Allow your Live Server
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+                .allowedOrigins("*") // Allow your Live Server
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS").allowedHeaders("*");
     }
 }
