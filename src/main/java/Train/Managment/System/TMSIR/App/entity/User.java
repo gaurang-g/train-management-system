ac4b10dc9ba5674e0c3497e96ab59b057383c071
j-A-b1 <jagbho3188@gmail.com>
2026-04-30 14:23:29 +0530

    Blank space issue is retain

--- src/main/java/Train/Managment/System/TMSIR/App/entity/User.java
+++ src/main/java/Train/Managment/System/TMSIR/App/entity/User.java
@@ -1,6 +1,7 @@
 package Train.Managment.System.TMSIR.App.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -16,8 +17,10 @@ public class User
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(unique = true,nullable = false)
+    @NotBlank(message = "Username cannot be blank")
     private String username;
     @Column(nullable = false,unique = true)
+    @NotBlank(message = "Password cannot be blank")
     private String password;
     private String role;
     private String fullName;
